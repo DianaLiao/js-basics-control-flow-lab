bd213f817c91af3d36cb920759989cdf33738238
Diana Liao <dijali@gmail.com>
2021-04-05 21:52:34 -0400

    commit tree b64ac200091f27757fcd2d0e3d8b9c23aa9fd9f4

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {return "This one is on me!"}
+  else if (distance > 2500) {return "No can do."}
+  else if (distance > 2000) {return "I will gladly take your thirty bucks."}
+  else {return "I did something wrong."}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default: 
+      return "Bye."
+  }
 }
